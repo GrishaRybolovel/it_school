27a123b1cfa6d97f51ee36bd0ea89525f7307adc
Григорий Рыболовлев <95378753+GrishaRybolovel@users.noreply.github.com>
2024-09-04 16:52:04 +0300

    Update handlers.py

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -6,12 +6,3 @@ btn_calc = KeyboardButton('Начать подсчет')
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 
 kb.add(btn_calc)
-
-
-keyboard_city = ReplyKeyboardMarkup()
-keyboard_city.add("Казань")
-keyboard_city.add("Нижнекамск")
-keyboard_city.add("Тольятти")
-keyboard_city.add("Москва")
-keyboard_city.add("Самара")
-keyboard_city.add("Назад")

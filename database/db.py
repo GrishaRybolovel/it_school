691d708f3bd811ab6daadd71dd4d284f2b79a03b
Григорий Рыболовлев <95378753+GrishaRybolovel@users.noreply.github.com>
2024-09-04 16:48:51 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,48 +1,49 @@
-import psycopg2
+import asyncpg
 import secrets
 import datetime
 
-
-def connect_sql():
-    global conn, cur
-    conn = psycopg2.connect("""
-    host=localhost
-    port=5432
-    dbname=postgres
-    user=postgres
-    """)
-
-    cur = conn.cursor()
-
-    if conn:
-        print('Database succesfully connected!')
-
-    # cur.execute("DROP TABLE IF EXISTS user")
-
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR, login VARCHAR);")
-    conn.commit()
+dbCredentials = {
+    'host': '28efd-rw.db.pub.dbaas.postgrespro.ru',
+    'port': '5432',
+    'database': 'dbstud',
+    'user': 'your_username',
+    'password': 'your_password'
+}
 
 
-def show_users():
-    cur.execute("SELECT * FROM users")
-    return cur.fetchall()
+class Database:
+    def __init__(self):
+        self.pool = None
+
+    async def connect(self):
+        self.pool = await asyncpg.create_pool(**dbCredentials)
+
+        query = "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR, phone_number VARCHAR)"
+        async with self.pool.acquire() as connection:
+            await connection.execute(query)
+        print('Успешное подключение')
+
+    async def disconnect(self):
+        await self.pool.close()
+
+    async def fetch_users(self):
+        query = "SELECT * FROM users"
+        async with self.pool.acquire() as connection:
+            return await connection.fetchall(query)
+
+    async def insert_user(self, name: str, phone_number: str):
+        query = "INSERT INTO users (name, phone_number) VALUES ($1, $2) RETURNING id"
+        async with self.pool.acquire() as connection:
+            user_id = await connection.fetchval(query, name, phone_number)
+            return user_id
 
 
-async def insert_user(data):
-    cur.execute(
-        'INSERT INTO users (name, login) VALUES (%s, %s)',
-        (data['name'], data['login']))
-    cur.execute('SELECT LASTVAL()')
-    lastid = cur.fetchone()[0]
-
-    conn.commit()
-    return lastid
-
-
-def main():
-    connect_sql()
-    print(show_users())
+async def main():
+    db = Database()
+    await db.connect()
+    users = await db.fetch_users()
+    print(users)
+    await db.disconnect()
 
 
 if __name__ == '__main__':
